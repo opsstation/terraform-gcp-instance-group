4f2f47a0c6128315ff859da2b34f68fa8de91b50
thekamleshyadav <thekamleshyadav26@gmail.com>
2024-02-08 15:12:33 +0530

    add github-actions

--- example/mig/healthcheck/versions.tf
+++ example/mig/healthcheck/versions.tf
@@ -1,17 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.6.6"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.4"
+      version = ">= 3.50, < 5.13.0"
     }
   }
 }
--- example/mig/simple/versions.tf
+++ example/mig/simple/versions.tf
@@ -1,13 +1,10 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.6.6"
+
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 4.0"
+      version = ">= 3.50, < 5.13.0"
     }
   }
 }
